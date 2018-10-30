8b2f2fb68e0fead07e156321910bb4dd91f66835
RobertoAlvarezMedina <robert10alvarz@gmail.com>
2018-10-30 12:38:07 -0600

    Agregar archivos de proyecto.

---
+++ BusinessLogic/PersonBL.cs
@@ -0,0 +1,36 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class PersonBL
+
+    {
+        List<Person> persons = new List<Person>();
+        public void Create(Person person)
+        {
+            if(string.IsNullOrEmpty(person.Name))
+            throw new ArgumentException("Name cannot be empty");
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        public Person GetById(int id)
+        {
+            foreach(var item in persons)
+            {
+                if (item.Id == id) return item;
+            }
+            return null;
+        }
+    }
+}
---
+++ Lab07.Tests/PersonTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+
+namespace Lab07.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+
+        [TestMethod]
+        public void APersonIsCreated()
+        {
+            //Arrange
+            PersonBL logic = new PersonBL();
+            Person person = new Person()
+            {
+                Id = 1,
+                Name = "John Smith",
+                BirthDate = new DateTime(1999, 1, 1)
+            };
+            //Act
+            logic.Create(person);
+            var lastInserted = logic.GetById(1);
+            //Assert
+            Assert.IsNotNull(lastInserted, "The last inserted is null.");
+
+        }
+
+        [TestMethod]
+        public void APersonMustHaveAName()
+        {
+            //Arrange
+            PersonBL logic = new PersonBL();
+            Person person = new Person()
+            {
+                Id = 1,
+                Name = "",
+                BirthDate = new DateTime(1999, 1, 1)
+            };
+
+            //Act (and assert)
+            Assert.ThrowsException<ArgumentException>(() => logic.Create(person));
+
+        }
+
+        [TestMethod]
+        public void TheAgeOfAPersonIsCalculatedCorrectly()
+        {
+            PersonBL logic = new PersonBL();
+            Person person = new Person()
+            {
+                Id = 1,
+                Name = "John Smith",
+                BirthDate = new DateTime(1999, 1, 1)
+            };
+            //Act
+            //Assert
+            Assert.IsNotNull(20,person.Age);
+        }
+
+    }
+}
